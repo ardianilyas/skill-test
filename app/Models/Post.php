458bb1548a3cf6318d31b5fca19b5f9fdd8787fc
ardianilyas <ardianilyas@gmail.com>
2025-12-18 20:05:31 +0700

    feat: add fillable, casts, user relation, and active scope to Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'is_draft',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'is_draft' => 'boolean',
+        'pusbliedh_at' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    #[Scope]
+    public function active(Builder $query): void
+    {
+        $query->where('is_draft', false)->whereNotNull('published_at')->where('published_at', '<=', now());
+    }
 }
