bcbeb0cfd5966c4af8ecac0db499d2fe5c123395
ardianilyas <ardianilyas@gmail.com>
2025-12-19 18:17:27 +0700

    feat: change store post request name (before is create post request)

--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CreatePostRequest extends FormRequest
+class StorePostRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -25,7 +25,7 @@ class CreatePostRequest extends FormRequest
             'title' => 'required|string|max:255',
             'content' => 'required|string',
             'is_draft' => 'boolean',
-            'published_at' => 'nullable|date',
+            'published_at' => 'nullable|date|after_or_equal:now',
         ];
     }
 }
